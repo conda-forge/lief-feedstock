96cc3a5ee8155b5ebc59738a5d4102246248eefe
Ray Donnelly <mingw.android@gmail.com>
2019-12-10 21:06:02 +0100

    Remove most debugging

--- recipe/install-liblief.sh
+++ recipe/install-liblief.sh
@@ -1,47 +1,7 @@
 #!/bin/bash
 
-# _ZTIN4LIEF5MachO16BuildToolVersionE => "typeinfo for LIEF::MachO::BuildToolVersion"
-# _ZTIN4LIEF5MachO12BuildVersionE => "typeinfo for LIEF::MachO::BuildVersion"
-
 ninja -v && ninja -v install
 
 if [[ -d "${PREFIX}"/share/LIEF/examples ]]; then
   rm -rf "${PREFIX}"/share/LIEF/examples/
-fi
-
-declare -a OBJDUMP=()
-if [[ ${target_platform} == osx-64 ]]; then
-  OBJDUMP+=(llvm-objdump)
-  V=$(llvm-objdump --version | grep version | cut -d' '  -f5 | cut -d'.' -f1)
-  OBJDUMP+=(-t)
-  if [[ ${V} == 4 ]]; then
-    # Want better matches for these.
-    LM="MachO"
-    LMBV="BuildVersion"
-    LMBTV="BuildToolVersion"
-  else
-    OBJDUMP+=(-C)
-    LM="typeinfo for LIEF::MachO::"
-    LMBV="typeinfo for LIEF::MachO::BuildVersion"
-    LMBTV="typeinfo for LIEF::MachO::BuildToolVersion"
-  fi
-else
-  OBJDUMP+=(objdump)
-  OBJDUMP+=(-T)
-  OBJDUMP+=(-C)
-  LM="typeinfo for LIEF::MachO::"
-  LMBV="typeinfo for LIEF::MachO::BuildVersion"
-  LMBTV="typeinfo for LIEF::MachO::BuildToolVersion"
-fi
-
-"${OBJDUMP[@]}" ${PREFIX}/lib/libLIEF${SHLIB_EXT} | rg "${LM}"
-
-if ! "${OBJDUMP[@]}" ${PREFIX}/lib/libLIEF${SHLIB_EXT} | rg "${LMBV}"; then
-  echo "Failed to find typeinfo for LIEF::MachO::BuildVersion in lib/libLIEF${SHLIB_EXT}"
-  exit 1
-fi
-
-if ! "${OBJDUMP[@]}" ${PREFIX}/lib/libLIEF${SHLIB_EXT} | rg "${LMBTV}"; then
-  echo "Failed to find typeinfo for LIEF::MachO::BuildToolVersion in lib/libLIEF${SHLIB_EXT}"
-  exit 2
 fi
