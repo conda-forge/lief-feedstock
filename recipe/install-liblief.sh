b68cabf005ae7fc208f2a75d0a29023984526c0b
Ray Donnelly <mingw.android@gmail.com>
2019-12-10 02:55:55 -0800

    More debugging

--- recipe/install-liblief.sh
+++ recipe/install-liblief.sh
@@ -6,14 +6,39 @@ if [[ -d "${PREFIX}"/share/LIEF/examples ]]; then
   rm -rf "${PREFIX}"/share/LIEF/examples/
 fi
 
-objdump -T -C ${PREFIX}/lib/libLIEF.so | rg "typeinfo for LIEF::MachO::"
+declare -a OBJDUMP=()
+if [[ ${target_platform} == osx-64 ]]; then
+  OBJDUMP+=(llvm-objdump)
+  V=$(llvm-objdump --version | grep version | cut -d' '  -f5 | cut -d'.' -f1)
+  OBJDUMP+=(-t)
+  if [[ ${V} == 4 ]]; then
+    # Want better matches for these.
+    LM="MachO"
+    LMBV="BuildVersion"
+    LMBTV="BuiltToolVersion"
+  else
+    OBJDUMP+=(-C)
+    LM="typeinfo for LIEF::MachO::"
+    LMBV="typeinfo for LIEF::MachO::BuildVersion"
+    LMBTV="typeinfo for LIEF::MachO::BuiltToolVersion"
+  fi
+else
+  OBJDUMP+=(objdump)
+  OBJDUMP+=(-T)
+  OBJDUMP+=(-C)
+  LM="typeinfo for LIEF::MachO::"
+  LMBV="typeinfo for LIEF::MachO::BuildVersion"
+  LMBTV="typeinfo for LIEF::MachO::BuiltToolVersion"
+fi
 
-if ! objdump -T -C ${PREFIX}/lib/libLIEF.so | rg "typeinfo for LIEF::MachO::BuildVersion"; then
+"${OBJDUMP[@]}" ${PREFIX}/lib/libLIEF.so | rg "${LM}"
+
+if ! "${OBJDUMP[@]}" ${PREFIX}/lib/libLIEF.so | rg "${LMBV}"; then
   echo "Failed to find typeinfo for LIEF::MachO::BuildVersion in lib/libLIEF.so"
   exit 1
 fi
 
-if ! objdump -T -C ${PREFIX}/lib/libLIEF.so | rg "typeinfo for LIEF::MachO::BuildToolVersion"; then
+if ! "${OBJDUMP[@]}" ${PREFIX}/lib/libLIEF.so | rg "${LMBTV}"; then
   echo "Failed to find typeinfo for LIEF::MachO::BuildToolVersion in lib/libLIEF.so"
   exit 2
 fi
