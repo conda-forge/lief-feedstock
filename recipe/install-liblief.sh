c4bdd2b645b2de8414988001e40aae4f3f33f0f4
Isuru Fernando <isuruf@gmail.com>
2020-07-18 14:02:22 -0500

    Go back to travis

--- recipe/install-liblief.sh
+++ recipe/install-liblief.sh
@@ -2,7 +2,7 @@
 
 pushd build
 
-ninja -v && ninja -v install
+ninja -j${CPU_COUNT} -v && ninja -v install
 
 if [[ -d "${PREFIX}"/share/LIEF/examples ]]; then
   rm -rf "${PREFIX}"/share/LIEF/examples/
