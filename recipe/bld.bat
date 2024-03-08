c2f5e4999e2a925b4e3ebcaa2af3ec41e925fb00
Marcel Bargull <marcel.bargull@udo.edu>
2024-03-08 18:35:33 +0100

    Suppress cl.exe logo and warnings C4251,C4275 to reduce log size
    (may want to address the warnings upstream).
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,9 +11,13 @@ set "CMAKE_ARGS=%CMAKE_ARGS% -DLIEF_PYTHON_API=OFF"
 
 mkdir build
 
+:: Suppresses warnings C4251, C4275 explicitly to reduce log size (may want to address this upstream).
+:: refs:
+::   - https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-1-c4251
+::   - https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275
 cmake %CMAKE_ARGS% -LAH -G "Ninja" -B build  ^
-    -DCMAKE_CXX_FLAGS="%CXXFLAGS% /EHsc"  ^
-    -DCMAKE_C_FLAGS="%CFLAGS% /EHsc"  ^
+    -DCMAKE_CXX_FLAGS="%CXXFLAGS% /nologo /EHsc /wd4251 /wd4275"  ^
+    -DCMAKE_C_FLAGS="%CFLAGS% /nologo /EHsc /wd4251 /wd4275"  ^
     -DCMAKE_VERBOSE_MAKEFILE=ON  ^
     -DCMAKE_C_USE_RESPONSE_FILE_FOR_OBJECTS=OFF
 
